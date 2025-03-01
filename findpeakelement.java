d664d8f27ede1befec8560835eff8900bfca5a7c
Tarak Aggarwal <116876777+Tarak1247@users.noreply.github.com>
2025-03-02 00:42:38 +0530

    Create findpeakelement.java

---
+++ findpeakelement.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;  // Peak is on the left
+            } else {
+                low = mid + 1;  // Peak is on the right
+            }
+        }
+        
+        return low;  // `low` or `high` will be at the peak
+    }
+}
